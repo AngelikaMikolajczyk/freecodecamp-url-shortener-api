68296511e0ecefa24d9d81af82e8cd5f73847e6d
Angelika Mikołajczyk <angelika.mikolajczyk11@gmail.com>
2020-11-27 12:33:59 +0100

    add hashing url address

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +18,25 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/api/shorturl/new', function(req, res){
+  let originalUrl = req.body.url;
+
+  // dns.lookup(originalUrl, function(err, address){
+  //   console.log(err);
+  //   if(err) {
+  //     res.json({error: 'invalid url'});
+  //   } else {
+  //     res.json({"original_url": originalUrl})
+  //   }
+  // })
+  
+  let hashedUrl = crypto.createHash('md5').update(originalUrl).digest('hex');
+
+  res.json({"original_url": originalUrl, "short_url": hashedUrl});
+  
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
